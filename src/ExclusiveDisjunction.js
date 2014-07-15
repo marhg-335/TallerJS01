4f0d5a812365bfaef5c04f56e0ef49ff89ae8d93
albarenguenatalia <albarenguenatalia@gmail.com>
2014-07-15 20:41:18 -0300

    Negation y ExclusiveDisjunction con exports

--- src/ExclusiveDisjunction.js
+++ src/ExclusiveDisjunction.js
@@ -1,11 +1,12 @@
 	
-function ExclusiveDisjunction( left, right){ 
+var ExclusiveDisjunction = exports.ExclusiveDisjunction = function ExclusiveDisjunction( left, right){ 
 	if (!left || !right ){
 		throw new Error("ExclusiveDisjunction: invalid operands!");
 	}
 	this.left = left;
 	this.right = right;	
-}
+};
+
 ExclusiveDisjunction.prototype.evaluation= function( assigments ){
 	p = this.left.evaluation( assigments);
 	q = this.right.evaluation(assigments);
--- src/Negation.js
+++ src/Negation.js
@@ -1,10 +1,11 @@
 
-function Negation(operand){	
+var Negation = exports.Negation = function Negation(operand){	
 	if( !operand ){
 		throw new Error("Negation: invalid operand!");
 	}
 	this.operand = operand;	
-}
+};
+
 Negation.prototype.evaluation = function( assigments){
 	return !(this.operand.evaluation(assigments));
 };
