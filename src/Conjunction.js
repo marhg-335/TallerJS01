6add9c7643cc85c4eb5a35d28605a80fd4c75323
albarenguenatalia <albarenguenatalia@gmail.com>
2014-07-21 20:23:03 -0300

    conjuntion - conjunction

--- src/Conjunction.js
+++ src/Conjunction.js
@@ -14,5 +14,5 @@ Conjunction.prototype.evaluation = function evaluation(assignments) {
 
 /** Metodo genera una proposicion y le aplica el operador de conjuncion. Para esto se toma en cuneta la altura minima y maxima*/
 exports.Conjunction.generate = function generate(random1, random2, min, max){
-     return new Conjuntion(Proposition.generate(random1, min-1, max-1), Proposition.generate(random2, min-1, max-1));
+     return new Conjunction(Proposition.generate(random1, min-1, max-1), Proposition.generate(random2, min-1, max-1));
 };
