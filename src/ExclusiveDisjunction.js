55b19c5971ee91f5ab06e3935ec90083b1aa47f6
albarenguenatalia <albarenguenatalia@gmail.com>
2014-07-21 17:43:28 -0300

    ExclusiveDisjunction y Negation con método generate y comentarios.
    Signed-off-by: albarenguenatalia <albarenguenatalia@gmail.com>

--- src/ExclusiveDisjunction.js
+++ src/ExclusiveDisjunction.js
@@ -1,5 +1,5 @@
-	
-var ExclusiveDisjunction = exports.ExclusiveDisjunction = function ExclusiveDisjunction( left, right){ 
+/**Constructor de ExclusiveDisjunction.*/
+exports.ExclusiveDisjunction = function ExclusiveDisjunction( left, right){ 
 	if (!left || !right ){
 		throw new Error("ExclusiveDisjunction: invalid operands!");
 	}
@@ -7,7 +7,14 @@ var ExclusiveDisjunction = exports.ExclusiveDisjunction = function ExclusiveDisj
 	this.right = right;	
 };
 
-ExclusiveDisjunction.prototype.evaluation= function( assigments ){
+/** Metodo generate para generar casos de prueba aleatorios*/
+ExclusiveDisjunction.generate( random, min, max){
+	return new Negation(Proposition.generate(random, min - 1, max - 1) );
+}
+
+/**Metodo evaluation definida en el prototype de ExclusiveDisjunction. 
+   Retorna un boolean con la disyuncion disjuntiva de la evaluacion de left y right*/
+exports.ExclusiveDisjunction.prototype.evaluation= function( assigments ){
 	p = this.left.evaluation( assigments);
 	q = this.right.evaluation(assigments);
 	return  p !== q;
--- src/Negation.js
+++ src/Negation.js
@@ -1,11 +1,18 @@
-
-var Negation = exports.Negation = function Negation(operand){	
+/**Constructor de Negation.*/
+exports.Negation = function Negation(operand){	
 	if( !operand ){
 		throw new Error("Negation: invalid operand!");
 	}
 	this.operand = operand;	
 };
 
-Negation.prototype.evaluation = function( assigments){
+/** Metodo generate para generar casos de prueba aleatorios*/
+Negation.generate( random, min, max ){
+	return new Negation(Proposition.generate(random, min - 1, max - 1) );
+}
+
+/**Metodo evaluation definida en el prototype de Negation. 
+   Retorna la negacion de la evaluacion de operand*/
+exports.Negation.prototype.evaluation = function( assigments){
 	return !(this.operand.evaluation(assigments));
 };
