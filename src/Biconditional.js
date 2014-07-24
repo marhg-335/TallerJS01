2d77f61e6b550c1a252532a60771afc981449379
jonaamigo <jonaamigo@hotmail.com>
2014-07-23 22:13:41 -0300

    Biconditional documentation Conditional documentation

--- src/Biconditional.js
+++ src/Biconditional.js
@@ -1,15 +1,19 @@
+/** Constructor Biconditional. */
 var Biconditional = exports.Biconditional = function Biconditional(left, right) {
+    if (!left || !right) {
+        throw new Error("Biconditional: invalid operands!");
+    }
     this.left = left;
     this.right = right;
 }
-
+/** Evaluation Biconditional */
 Biconditional.prototype.evaluation = function evaluation(assignments) {
     var resLeft = this.left.evaluation(assignments);
     var resRight = this.right.evaluation(assignments);
 
     return (resLeft === resRight);
 }
-
+/** Generate Biconditional */
 Biconditional.generate = function generate( random, min, max){
 	return new Biconditional(Proposition.generate(random, min - 1, max - 1), Proposition.generate(random, min -1, max - 1) );
 };
--- src/Conditional.js
+++ src/Conditional.js
@@ -1,16 +1,21 @@
-function Conditional(left,right){
+/** Constructor Conditional. */
+function Conditional(left, right) {
+    if (!left || !right) {
+        throw new Error("Conditional: invalid operands!");
+    }
     this.left = left;
     this.right = right;
 }
-
-Conditional.generate = function generate( random, min, max){
-	return new Conditional(Proposition.generate(random, min - 1, max - 1), Proposition.generate(random, min -1, max - 1) );
-};
+/** Evaluation Conditional */
 Conditional.prototype.evaluation = function evaluation(assignments) {
     var resLeft = this.left.evaluation(assignments);
     var resRight = this.right.evaluation(assignments);
 
-    return resLeft && !resRight;
+    return !resLeft || resRight;
 }
+/** Generate Conditional */
+Conditional.generate = function generate(random, min, max) {
+    return new Conditional(Proposition.generate(random, min - 1, max - 1), Proposition.generate(random, min - 1, max - 1));
+};
 
 
