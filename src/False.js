cdbb81540eb56b8feb8f52cc111b42c052a92389
CynthiaBarindelli <cynthia.barindelli@outlook.com>
2014-08-01 17:43:51 -0300

    Disjunction True False

--- src/False.js
+++ src/False.js
@@ -3,11 +3,11 @@ var False = exports.False = declare(Proposition,{
 	constructor:function False(){
 	Proposition.call(this);
 	//do nothing
-};
+},
 /** Metodo que retorna si una evaluacion es False. */
 evaluation : function evaluation(){
 	return false;
-};
+},
 /**Metodo generate que retorna false. */
 'static generate': function generate(random,min,max){
 	return new False();
