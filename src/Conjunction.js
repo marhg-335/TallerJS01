205ba868fb37f87a5786316613344ca038b9e9a5
marhg_335 <martingon4eo@gmail.com>
2014-08-01 19:07:16 -0300

    cambios

--- src/Conjunction.js
+++ src/Conjunction.js
@@ -16,10 +16,6 @@ var Conjunction = exports.Conjunction = declare(Proposition, {
 	},
 
 	/** Metodo genera una proposicion y le aplica el operador de conjuncion. Para esto se toma en cuneta la altura minima y maxima*/
-<<<<<<< HEAD
-	'static generate' : function generate(random1, min, max){
-	     return new Conjunction(Proposition.generate(random1, min-1, max-1), Proposition.generate(random2, min-1, max-1));
-=======
 	'static generate' : function generate(random, min, max){
 	     return new Conjunction(Proposition.generate(random, min-1, max-1), Proposition.generate(random, min-1, max-1));
 	},
@@ -27,6 +23,5 @@ var Conjunction = exports.Conjunction = declare(Proposition, {
 	/** Retoran lista de variables en el sub arbol*/
 	'variables' : function variables(){
 		return union(this.left.variables(), this.right.variables())
->>>>>>> Agrego variables en conjuntion y variable
 	}
 });
