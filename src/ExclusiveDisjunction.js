77a06e552e4d0390abace3a543091f83e9345e63
albarenguenatalia <albarenguenatalia@gmail.com>
2014-08-01 17:29:07 -0300

    Se agregó prototipo para usar declare, orientado a objetos.

--- src/ExclusiveDisjunction.js
+++ src/ExclusiveDisjunction.js
@@ -1,22 +1,28 @@
 /**Constructor de ExclusiveDisjunction.*/
-var ExclusiveDisjunction = exports.ExclusiveDisjunction = function ExclusiveDisjunction( left, right){ 
-	if (!left || !right ){
-		throw new Error("ExclusiveDisjunction: invalid operands!");
-	}
-	this.left = left;
-	this.right = right;	
-};
+var ExclusiveDisjunction = exports.ExclusiveDisjunction = declare( Proposition, {
 
-/**Retorna un booleano con el resultado de aplicar el operador de disyuncion exclusiva
-   a la evaluacion de left y right*/
-ExclusiveDisjunction.prototype.evaluation= function( assigments ){
-	var p = this.left.evaluation( assigments);
-	var q = this.right.evaluation(assigments);
-	return  p !== q;
-};
-
-/** Metodo generate para generar casos de prueba aleatorios.
+	/**Constructor de ExclusiveDisjunction.*/
+	constructor: function ExclusiveDisjunction( left, right){ 
+		if (!left || !right ){
+			throw new Error("ExclusiveDisjunction: invalid operands!");
+		}
+		this.left = left;
+		this.right = right;	
+	},
+	/**Retorna un booleano con el resultado de aplicar el operador de disyuncion exclusiva
+   	a la evaluacion de left y right*/
+	evaluation: function evaluation( assigments ){
+		var p = this.left.evaluation( assigments);
+		var q = this.right.evaluation(assigments);
+		return  p !== q;
+	},
+	/** Metodo generate para generar casos de prueba aleatorios.
 	Retorna un objeto ExclusiveDisjunction con operandos left y right como objetos Proposition aleatorios.*/
-ExclusiveDisjunction.generate = function generate( random, min, max){
-	return new ExclusiveDisjunction(Proposition.generate(random, min - 1, max - 1), Proposition.generate(random, min -1, max - 1) );
-};
+	'static generate': function generate( Random, min, max ){
+		return new ExclusiveDisjunction(Proposition.generate(Random, min - 1, max - 1), Proposition.generate(Random, min -1, max - 1) );
+	}
+
+
+
+});
+
--- src/Negation.js
+++ src/Negation.js
@@ -1,20 +1,22 @@
-/**Constructor de Negation.*/
-var Negation = exports.Negation = function Negation(operand){	
-	if( !operand ){
-		throw new Error("Negation: invalid operand!");
-	}
-	this.operand = operand;	
-};
 
-/**Retorna un booleano con el resultado de aplicar el operador de negacion a la evaluacion de operand*/
-Negation.prototype.evaluation = function( assigments){
-	return !(this.operand.evaluation(assigments));
-};
-
-/** Metodo generate para generar casos de prueba aleatorios
+var Negation = exports.Negation = declare(Proposition, {
+	/**Constructor de Negation.*/
+	constructor: function Negation(operand){	
+		Proposition.call(this);
+		if( !operand ){
+			throw new Error("Negation: invalid operand!");
+		}
+		this.operand = operand;	
+	},
+	/**Retorna un booleano con el resultado de aplicar el operador de negacion a la evaluacion de operand*/
+	evaluation: function evaluation( assigments){
+		return !(this.operand.evaluation(assigments));
+	},
+	/** Metodo generate para generar casos de prueba aleatorios
 	Retorna un objeto Negation con operand un objeto Proposition aleatorio*/
-Negation.generate = function generate( random, min, max ){
-	return new Negation(Proposition.generate(random, min - 1, max - 1) );
-};
+	'static generate': function generate( Random, min, max ){
+		return new Negation(Proposition.generate(Random, min - 1, max - 1) );
+	}
 
+});
 
